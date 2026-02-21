635afad10f50cc7c3f4b8070f830bdcdb9212100
João Pedro <fjoaopedro1302@gmail.com>
2026-02-21 16:21:18 -0300

    fix: calculo de produção de construção pelo efeitos ativos

--- src/game.cpp
+++ src/game.cpp
@@ -1,5 +1,6 @@
 #include <game.hpp>
 #include "./data/builds/builds_data.hpp"
+#include "./data/upgrades/upgrades_data.hpp"
 
 typedef unsigned long long ull;
 
@@ -10,25 +11,17 @@ ull Game::calc_build_production(const Build &build) const {
     ull base_prod = data->base_prod;
     int quantity = build.quantity;
 
-    int upgrade_multipliers = 100;
-
-    for (const Upgrade &purchased_upgrade : purchased_upgrades) {
-        for (const Effect &effect : purchased_upgrade.effects) {
-            if (effect.target == EFFECT_TARGET::GLOBAL_PROD || (effect.target == EFFECT_TARGET::BUILD && effect.target_id == build_id)) {
-                upgrade_multipliers *= effect.value;
-            }
-        }
-    }
-
     int effect_multipliers = 100;
 
-    for (const Effect &active_effect : active_effects) {
-        if (active_effect.target == EFFECT_TARGET::GLOBAL_PROD || (active_effect.target == EFFECT_TARGET::BUILD && active_effect.target_id == build_id)) {
-            effect_multipliers *= active_effect.value;
+    for (const Effect &effect : active_effects) {
+        const EffectData *data = effect.data;
+
+        if (data->target == EffectTarget::GLOBAL_PRODUCTION || (data->target == EffectTarget::BUILD && data->target_id == build_id)) {
+            effect_multipliers *= effect.value;
         }
     }
      
-    return base_prod * quantity * upgrade_multipliers * effect_multipliers / 10000;
+    return base_prod * quantity * effect_multipliers / 100;
 }
 
 ull Game::calc_total_production() const {
