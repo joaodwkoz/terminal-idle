56d6ca0f6f177052e036134c73d1be8ef242c9d6
João Pedro <fjoaopedro1302@gmail.com>
2026-02-20 21:25:11 -0300

    feat: novos atributos e nova abordagem de lidar com os upgrades comprados em game

--- include/game.hpp
+++ include/game.hpp
@@ -2,8 +2,6 @@
 
 #include <string>
 #include <vector>
-#include "./shop.hpp"
-#include "./upgrade.hpp"
 #include "./effect.hpp"
 #include "./build.hpp"
 
@@ -11,13 +9,13 @@ typedef unsigned long long ull;
 
 struct Game {
     std::string name;
-    ull score;
-    Shop shop;
-    std::vector<Upgrade> purchased_upgrades;
+    ull bits;
+    int ticks;
+    std::vector<bool> purchased_upgrades;
     std::vector<Effect> active_effects;
     std::vector<Build> builds;
 
-    Game(const std::string &name, ull score, Shop shop, const std::vector<Upgrade> &purchased_upgrades, const std::vector<Effect> &active_effects, const std::vector<Build> &builds) : name(name), score(score), shop(shop), purchased_upgrades(purchased_upgrades), active_effects(active_effects), builds(builds) {}
+    Game(const std::string &name, ull bits, int ticks, const std::vector<bool> &purchased_upgrades, const std::vector<Effect> &active_effects, const std::vector<Build> &builds) : name(name), bits(bits), ticks(ticks), purchased_upgrades(purchased_upgrades), active_effects(active_effects), builds(builds) {}
 
     ull calc_build_production(const Build& build) const;
 
