7babc3ab18ad2f9d50c1cdcb420ee73e52410295
João Pedro <fjoaopedro1302@gmail.com>
2026-02-22 22:02:35 -0300

    fix: vetor com todos os effects declarado externamente

--- src/data/effects/effects_data.hpp
+++ src/data/effects/effects_data.hpp
@@ -15,3 +15,5 @@ struct EffectData {
 
     EffectData(int id, EffectType type, EffectTarget target, int target_id, const std::string &name, const std::string &desc, int base_value, int base_duration) : id(id), type(type), target(target), target_id(target_id), name(name), desc(desc), base_value(base_value), base_duration(base_duration) {}
 };
+
+extern const std::vector<EffectData> EFFECTS;
