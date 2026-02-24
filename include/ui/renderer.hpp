b35a165d01eda29a6e581e69e9db453f6cec2b6f
João Pedro <fjoaopedro1302@gmail.com>
2026-02-24 16:45:34 -0300

    feat: classe renderer do terminal

---
+++ include/ui/renderer.hpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <windows.h>
+
+class Renderer {
+    private:
+        std::vector<std::string> back_buffer;
+        std::vector<std::string> front_buffer;
+
+        void get_terminal_size(int &width, int &height) const;
+    public:
+        Renderer() {
+            int width;
+            int height;
+
+            get_terminal_size(width, height);
+        
+            back_buffer.assign(height, std::string(width, 0));
+            front_buffer.assign(height, std::string(width, 0));
+
+            std::cout << "\033[?25l";
+        }
+        
+        void clear();
+
+        void draw(int x, int y, char c);
+
+        void draw_string(int x, int y, std::string str);
+
+        void render();
+};
