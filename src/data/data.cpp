3b2f4242cd04bd826d30198b44de2181f2f51dfa
João Pedro <fjoaopedro1302@gmail.com>
2026-03-16 18:06:15 -0300

    fix: remoção de qualificação redundante de data

--- src/data/data.cpp
+++ src/data/data.cpp
@@ -1,15 +1,15 @@
 #include "data/data.hpp"
 
 namespace data {
-    bool data::is_valid_build_id(const int id) {
+    bool is_valid_build_id(const int id) {
         return id >= 0 && id < (int) BUILDS.size();
     }
 
-    bool data::is_valid_effect_id(const int id) {
+    bool is_valid_effect_id(const int id) {
         return id >= 0 && id < (int) EFFECTS.size();
     }
 
-    bool data::is_valid_upgrade_id(const int id) {
+    bool is_valid_upgrade_id(const int id) {
         return id >= 0 && id < (int) UPGRADES.size();
     }
 }
